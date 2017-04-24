cba3c184c6cbd4242f4db2f354e2740c77f53796
Eliyahu <widi9545@colorado.edu>
2017-04-23 23:42:18 -0600

    Got the gameboard to draw!

--- src/dnm/GameBoard.java
+++ src/dnm/GameBoard.java
@@ -6,30 +6,60 @@ import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import javax.swing.*;
+import java.awt.*;
+
 
 public class GameBoard {
-	public static void main(String[] args) throws IOException {
-		JFrame frame = buildFrame();
-		final BufferedImage image = ImageIO.read(new File("board.jpg"));
-		
-		JPanel pane = new JPanel(){
-			@Override
-			protected void paintComponent(Graphics g){
-				super.paintComponent(g);
-				g.drawImage(image, 0, 0, null);			
-			}
-		};
-		frame.add(pane);
-		
+	
+	private MonopolyPanel newPane;
+	
+	private void displayGUI(){
+		JFrame frame = new JFrame("GameBoard");
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		newPane = new MonopolyPanel();
+		frame.setContentPane(newPane);
+		frame.pack();
+		frame.setLocationByPlatform(true);
+		frame.setVisible(true);
 	}
 	
-	private static JFrame buildFrame(){
-		JFrame frame = new JFrame();
-		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		frame.setSize(400, 400);
-		frame.setVisible(true);
-		return frame;
-	}
 	
 
+	private class MonopolyPanel extends JPanel {
+		private BufferedImage gameBoard;
+		public MonopolyPanel() {
+			try{ 
+			gameBoard = ImageIO.read(MonopolyPanel.class.getResource("board.jpg"));
+		} catch (IOException ioe){
+			ioe.printStackTrace();
+		}
+		
+	}
+		@Override
+		public Dimension getPreferredSize(){
+			return gameBoard == null ? new Dimension(100, 100): new Dimension(800, 818);
+		}
+		
+		@Override
+		protected void paintComponent(Graphics g){
+			super.paintComponent(g);
+			g.drawImage(gameBoard, 0, 0, this);
+		}
+	}
+	
+	public static void main(String[] args){
+		Runnable runnable = new Runnable(){
+			public void run(){
+				new GameBoard().displayGUI();
+			}
+			};
+			EventQueue.invokeLater(runnable);
+		
+	}
+	
 }
+		
+
+
+
